a7486d43117bd9728058b663b528fc39f495971e
flaviofogaca <flavio.eli22@live.com>
2025-04-02 20:57:12 -0300

    Ajustados Allowed_hosts no .env e conigurado CORS_ALLOW no settings.py

--- backend/core/models.py
+++ backend/core/models.py
@@ -6,8 +6,8 @@ from django.db import models
 
 
 class Base(models.Model):
-    criado_em = models.DateTimeField()
-    atualizado_em = models.DateTimeField()
+    criado_em = models.DateTimeField(auto_now_add=True)
+    atualizado_em = models.DateTimeField(auto_now_add=True)
 
     class Meta:
         abstract = True
